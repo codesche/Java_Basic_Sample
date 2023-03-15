75f5d6d82efcf0d9573e6283d34176ab34a5bc43
codesche <110509654+codesche@users.noreply.github.com>
2023-03-15 14:41:56 +0900

    Add files via upload

---
+++ Exercise117.java
@@ -0,0 +1,14 @@
+
+
+public class Exercise117 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+
+        int nextx = ++x;
+        int nexty = y++;
+
+        System.out.println(nextx);
+        System.out.println(nexty);
+    }
+}
---
+++ Exercise124.java
@@ -0,0 +1,15 @@
+
+
+public class Exercise124 {
+
+    static int getTotalPage(int m, int n) {
+        return m / n + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getTotalPage(5, 10));
+        System.out.println(getTotalPage(15, 10));
+        System.out.println(getTotalPage(25, 10));
+        System.out.println(getTotalPage(30, 10));
+    }
+}

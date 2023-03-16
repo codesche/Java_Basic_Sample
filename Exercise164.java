27653a6f3e59bc272941480f1d1b447cf6df25c5
codesche <110509654+codesche@users.noreply.github.com>
2023-03-16 17:20:22 +0900

    Add files via upload

---
+++ Exercise164.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Exercise164 {
+    public static void main(String[] args) {
+        // 컬렉션 생성
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("벤츠");
+        cars.add("람보르기니");
+        cars.add("롤스로이스");
+        cars.add("페라리");
+
+        // iterator 획득
+        Iterator<String> iterator = cars.iterator();
+
+        // while문을 사용한 경우
+        while (iterator.hasNext()) {
+            String str = iterator.next();
+            System.out.println(str);
+        }
+
+        // for-each문을 사용한 경우
+        for (String str : cars) {
+            System.out.println(str);
+        }
+
+    }
+}
